98c523b90cc587e03a5939e4bc4efc9525e7c26f
sekka1 <garlandk@gmail.com>
2011-01-26 11:19:31 -0800

    added folders for the iphone so that the iphone emulator tab shows up in Appcelerator.

--- build/android/gen/smurf/grepr/iv/SmurfAppInfo.java
+++ build/android/gen/smurf/grepr/iv/SmurfAppInfo.java
@@ -28,7 +28,7 @@ public final class SmurfAppInfo implements ITiAppInfo
 	}
 	
 	public String getVersion() {
-		return "1.0.1011";
+		return "1.0.1095";
 	}
 	
 	public String getPublisher() {
---
+++ build/iphone/Classes/TiRootViewController.h
@@ -0,0 +1,56 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ * 
+ * WARNING: This is generated code. Modify at your own risk and without support.
+ */
+
+#import "TiRootController.h"
+#import "TiWindowProxy.h"
+
+
+#define MAX_ORIENTATIONS	7
+
+@interface TiRootViewController : UIViewController<UIApplicationDelegate,TiRootController,TiOrientationController> {
+@private
+	NSMutableArray *windowViewControllers;	
+	
+	UIColor * backgroundColor;
+	UIImage * backgroundImage;
+
+	TiOrientationFlags	allowedOrientations;
+	NSTimeInterval	orientationRequestTimes[MAX_ORIENTATIONS];
+
+	UIInterfaceOrientation lastOrientation;
+	UIInterfaceOrientation windowOrientation;
+
+	NSMutableArray * viewControllerStack;
+	BOOL isCurrentlyVisible;
+	
+	//TiOrientationController variables.
+	NSMutableArray * windowProxies;
+}
+
+@property(nonatomic,readwrite,retain)	UIColor * backgroundColor;
+@property(nonatomic,readwrite,retain)	UIImage * backgroundImage;
+
+-(UIViewController *)focusedViewController;
+
+-(void)windowFocused:(UIViewController*)focusedViewController;
+-(void)windowClosed:(UIViewController *)closedViewController;
+
+-(CGRect)resizeView;
+-(void)repositionSubviews;
+
+-(void)manuallyRotateToOrientation:(UIInterfaceOrientation)orientation;
+-(void)manuallyRotateToOrientation:(UIInterfaceOrientation)newOrientation duration:(NSTimeInterval)duration;
+
+-(void)setOrientationModes:(NSArray *)newOrientationModes;
+
+- (void)openWindow:(TiWindowProxy *)window withObject:(id)args;
+- (void)closeWindow:(TiWindowProxy *)window withObject:(id)args;
+
+
+@end
