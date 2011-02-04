f39db6aa74c9c64ac0bdf303a52bec1a13901b94
sekka1 <garlandk@gmail.com>
2011-02-04 11:16:29 -0800

    added the 2 arbitrary pages

---
+++ Resources/main_windows/arbitrary.js
@@ -0,0 +1,82 @@
+var win = Titanium.UI.currentWindow;  
+
+var btnBack = Titanium.UI.createButton({  
+    title:'Back',  
+    top:10,  
+    left:20,
+    width:75,  
+    height:20,
+    borderRadius:1,  
+    font:{fontFamily:'Arial',fontWeight:'bold',fontSize:14}  
+});  
+win.add(btnBack);
+
+btnBack.addEventListener('click', function()
+{
+   win.windowHome.open();
+   win.close();
+});
+
+
+Ti.API.info( "arbitraryWinID: " + win.arbitraryWinID  );
+
+var textName = '';
+var textDescription = '';
+
+
+if( win.arbitraryWinID == 0 ){
+  
+    //Ti.API.info( "textAboutUs: " + win.textAboutUs  );
+    
+    textName = 'About Us';
+    textDescription = win.textAboutUs;
+
+}
+if( win.arbitraryWinID == 1 ){
+    //Ti.API.info( "textWeddingParty: " + win.textWeddingParty  );
+
+    textName = 'Wedding Party';
+    textDescription = win.textWeddingParty;
+}
+
+
+    var scrollView1 = Titanium.UI.createScrollView({
+        contentWidth:'auto',
+        contentHeight:'auto',
+        top:40,
+        left:10,
+        //width:300,
+        //height:600,
+        borderRadius:10,
+        backgroundColor:'#ff99000',
+        showVerticalScrollIndicator:true,
+        showHorizontalScrollIndicator:false
+    });
+    
+    var eventName = Titanium.UI.createLabel({  
+        text:textName,  
+        top:10,  
+        left:10,  
+        width:300,  
+        height:'auto',
+        backgroundColor:'#336699'  
+    });  
+    scrollView1.add(eventName);  
+        
+    var eventDescription = Titanium.UI.createLabel({  
+        text:textDescription,  
+        top:30,  
+        left:10,  
+        width:300,  
+        height:'auto',
+        backgroundColor:'#336699'  
+    });  
+    scrollView1.add(eventDescription); 
+    
+    win.add( scrollView1 );
+
+
+
+
+
+
