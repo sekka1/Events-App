d2a7d39b7f4c75aac1ae8950e20275d2c3a64a06
sekka1 <garlandk@gmail.com>
2011-02-14 23:22:50 -0800

    Changed properties file.  Changed to new image post location.

--- Resources/main_windows/properties.js
+++ Resources/main_windows/properties.js
@@ -1,5 +1,5 @@
 
 var baseURL = "http://www.myapp.com/";
-var setPostPhotoURL = "http://smurf.grep-r.com/photos/upload";
+var setPostPhotoURL = "http://smurf.grep-r.com/photosguests/upload";
 Titanium.App.Properties.setString("baseURL",baseURL);
 Titanium.App.Properties.setString("postPhotoURL",setPostPhotoURL);
