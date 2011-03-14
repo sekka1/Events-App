ece300d4d3b2b28fa6f4948fb746955a81004029
sekka1 <garlandk@gmail.com>
2011-03-13 23:48:57 -0700

    Reverted Gift registry page.  Added the login button in the comment page.

--- Resources/main_windows/giftRegistry.js
+++ Resources/main_windows/giftRegistry.js
@@ -38,9 +38,9 @@ btnBack.addEventListener('click', function()
 
 
 var webview = Titanium.UI.createWebView({
-    //url:'http://www1.macys.com/registry/wedding/registryhome?cm_mmc=Google_Bridal-_-Coremetrics+Bridal+Exact_gift+registry-_-4181574038_Exact-_-gift+registry_mkwid_s100000000000236374430_4181574038|-|100000000000236374430&cm_guid=1-_-100000000000236374430-_-4181574038',
+    url:'http://www1.macys.com/registry/wedding/registryhome?cm_mmc=Google_Bridal-_-Coremetrics+Bridal+Exact_gift+registry-_-4181574038_Exact-_-gift+registry_mkwid_s100000000000236374430_4181574038|-|100000000000236374430&cm_guid=1-_-100000000000236374430-_-4181574038',
     //url:'http://zend3.grep-r.com/gkan/gar-comment-2.php',
-    url:'http://smurf.grep-r.com/photos/fb/width/450/location/smurf.grep-r.com/url/__pictures__14-phpRxT0MO.png',
+    //url:'http://smurf.grep-r.com/photos/fb/width/450/location/smurf.grep-r.com/url/__pictures__14-phpRxT0MO.png',
     top:40
 });
 
