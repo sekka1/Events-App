4e836f8881e0892e67087a8ebe31c6c9d5304ed5
sekka1 <garlandk@gmail.com>
2011-04-15 14:22:22 -0700

    Changed manifest to use wedvite instead of grepr

--- Resources/main_windows/arbitrary.js
+++ Resources/main_windows/arbitrary.js
@@ -49,37 +49,77 @@ win.loader.open( "GET", win.site_url + "data/index/class/GetArbitraryInfo/method
 
 win.loader.onload = function() 
 {
+	Ti.API.info( this.responseText );
+	
 	results = eval('('+this.responseText+')');
+	
+	var scrollView1 = Titanium.UI.createScrollView({
+		contentWidth:'auto',
+		contentHeight:'auto',
+		top:40,
+		left:0,
+		//width:300,
+		//height:600,
+		borderRadius:0,
+		backgroundColor:'#336699',
+		showVerticalScrollIndicator:true,
+		showHorizontalScrollIndicator:false
+	});
 
 	if( win.arbitraryWinID == 0 ){
+		// About Us Section
 	  
 		//Ti.API.info( "textAboutUs: " + win.textAboutUs  );
 		
+		var questionsAnswers = results[0].description;
+		var qa = questionsAnswers.split("+-+-+-+-+-+-");
+		
 		textName = 'About Us';
-		textDescription = results[0].description;
+		
+		// Question 1
+		var q1 = Titanium.UI.createLabel({  
+			text:qa[0],  
+			top:30,  
+			//left:0,  
+			//width:300,  
+			//height:'auto',
+			backgroundColor:'#336699'  
+		});  
+		scrollView1.add(q1); 
+		
+		var a1 = Titanium.UI.createLabel({  
+			text:qa[1],  
+			//top:q1.height + 20,  
+			//left:0,  
+			//width:300,  
+			//height:'auto',
+			backgroundColor:'#336699'  
+		});  
+		scrollView1.add(a1); 
+		
+		// Line 1
+		var line1 = Ti.UI.createLabel({
+			//height:2,
+			//width:300,
+			//top:q1.height + a1.height + 10,
+			//left:1,
+			backgroundColor:'#000'
+		});
+		//scrollView1.add(line1); 
+		
+		Ti.API.info( "heights: " + q1.height + " - " + a1.height );
 	
 	}
 	if( win.arbitraryWinID == 1 ){
+		// Family Section
+	
 		//Ti.API.info( "textWeddingParty: " + win.textWeddingParty  );
 	
 		textName = 'Family';
 		textDescription = results[1].description;;
 	}
 	
-	
-		var scrollView1 = Titanium.UI.createScrollView({
-			contentWidth:'auto',
-			contentHeight:'auto',
-			top:40,
-			left:0,
-			//width:300,
-			//height:600,
-			borderRadius:0,
-			backgroundColor:'#336699',
-			showVerticalScrollIndicator:true,
-			showHorizontalScrollIndicator:false
-		});
-		
+
 		var eventName = Titanium.UI.createLabel({  
 			text:textName,  
 			top:10,  
@@ -88,7 +128,7 @@ win.loader.onload = function()
 			height:'auto' 
 		});  
 		win.add(eventName);  
-			
+/*			
 		var eventDescription = Titanium.UI.createLabel({  
 			text:textDescription,  
 			top:30,  
@@ -98,7 +138,7 @@ win.loader.onload = function()
 			backgroundColor:'#336699'  
 		});  
 		scrollView1.add(eventDescription); 
-		
+*/	
 		win.add( scrollView1 );
 
 }
