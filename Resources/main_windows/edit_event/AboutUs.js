f47d165b6eb54cc87675cae29b1ab6a171ad2af1
sekka1 <garlandk@gmail.com>
2011-06-14 20:38:42 -0700

    Changed the edit pages to use the new nav and back buttons

--- Resources/main_windows/edit_event/AboutUs.js
+++ Resources/main_windows/edit_event/AboutUs.js
@@ -5,7 +5,7 @@ var nav_bar = Titanium.UI.createImageView({
         top:0,
         left:0,
         height:40,
-        width:480,
+        //width:480,
 	    borderWidth: 0,
 	    borderRadius: 0
 });
@@ -13,11 +13,12 @@ win.add(nav_bar);
 
 var btnBack = Titanium.UI.createButton({  
     title:'',  
-    backgroundImage:'../../templates/multi-color/navigation/back.png',
-    top:5,  
+    backgroundImage:'../../images/templates/multi-color/back.png',
+    backgroundSelectedImage: '../../images/templates/multi-color/back_over.png',
+    top:2,  
     left:2,
-    width:35,  
-    height:35,
+    width:65,  
+    height:40,
     borderRadius:1,  
     font:{fontFamily:'Arial',fontWeight:'bold',fontSize:14}  
 });  
