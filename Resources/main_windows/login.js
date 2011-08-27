af30399b891b351b9b77d3f6cfefa41c8a100acc
sekka1 <garlandk@gmail.com>
2011-08-26 18:56:54 -0700

    Change it so that the facebook login in the login screen does not automatically pop up

--- Resources/main_windows/login.js
+++ Resources/main_windows/login.js
@@ -239,7 +239,7 @@ function displayInvitedToWeddingList(){
 
 Titanium.Facebook.appid = '197440486945083';
 Titanium.Facebook.permissions = ['user_status' ,'publish_stream', 'user_photos', 'friends_photos', 'friends_status', 'user_videos', 'friends_videos', 'read_stream', 'read_friendlists', 'manage_friendlists', 'read_requests']; // Permissions your app needs
-Titanium.Facebook.authorize();
+//Titanium.Facebook.authorize(); // If this is uncommented the facebook login would automatically pop up
 Titanium.Facebook.addEventListener('login', function(e) {
     if (e.success) {
         Ti.API.info( 'Logged In as: ' + Titanium.Facebook.uid );
